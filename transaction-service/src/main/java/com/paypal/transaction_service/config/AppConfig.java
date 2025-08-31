d956d0e8c514531d0b932bfe0397f105ae273528
Ganesh Shet <ganesh.shet@volvo.com>
2025-08-31 21:49:07 +0530

    kafka

--- transaction-service/src/main/java/com/paypal/transaction_service/config/AppConfig.java
+++ transaction-service/src/main/java/com/paypal/transaction_service/config/AppConfig.java
@@ -1,5 +1,9 @@
 package com.paypal.transaction_service.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
 @Configuration
 public class AppConfig {
     @Bean
